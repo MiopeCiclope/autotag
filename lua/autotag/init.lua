013d7afd38e7d69594c729adfd09d93be332daa1
Romulo Tone <romulo.tone@etraveligroup.com>
2024-06-04 15:24:20 +0200

    it aint working

--- lua/autotag/init.lua
+++ lua/autotag/init.lua
@@ -1,8 +1,58 @@
 local M = {}
 
-function M.print_message()
-    print("Neoroma being founded")
+M.print_message = function()
+  print("Neoroma being founded")
 end
 
-return M
+local function findLastTag(text)
+  local lastTag = nil
+  for tag in text:gmatch("<(%w+([%.%w+]*))[^>]*") do
+    lastTag = tag
+  end
+  return lastTag
+end
 
+local function extract_last_html_tag()
+  local text = vim.api.nvimMet_current_line()
+
+  local tag = findLastTag(text)
+  if tag then
+    -- Get the current cursor position
+    local row, col = unpack(vim.api.nvim_winMet_cursor(0))
+
+    local insert_text = "></" .. tag .. ">"
+    vim.schedule(function()
+      vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { insert_text })
+      vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+    end)
+    return ""
+  else
+    return ">"
+  end
+end
+
+M.setupAutoTag = function()
+  print("it triggers")
+  vim.api.nvim_buf_set_keymap(0, "i", ">", "v:lua.extract_last_html_tag()", { noremap = true, expr = true })
+end
+
+function M.setup()
+  vim.api.nvim_create_augroup("test", { clear = true })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = "test",
+    pattern = "typescriptreact",
+    callback = function()
+      vim.api.nvim_buf_set_keymap(0, "n", "<leader>tt", "lua require('autotag').print_message()<CR>", {})
+    end,
+  })
+end
+
+-- vim.cmd([[
+--         augroup jsx_tsx_mappings
+--           autocmd!
+--           autocmd FileType typescriptreact,javascriptreact,html lua M.setupAutoTag()
+--         augroup END
+--       ]])
+--
+return M
