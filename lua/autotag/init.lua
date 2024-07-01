19348cdbb196e266463a10cea05a4233b4fce1f8
Romulo Tone <romulo.tone@etraveligroup.com>
2024-07-01 10:42:12 +0200

    add options for file patterns

--- lua/autotag/init.lua
+++ lua/autotag/init.lua
@@ -1,48 +1,51 @@
 local M = {}
 
 local function findLastTag(text)
-  local lastTag = nil
-  for tag in text:gmatch("<(%w+([%.%w+]*))[^>]*") do
-    lastTag = tag
-  end
-  return lastTag
+	local lastTag = nil
+	for tag in text:gmatch("<(%w+([%.%w+]*))[^>]*") do
+		lastTag = tag
+	end
+	return lastTag
 end
 
 local function extract_last_html_tag()
-  local text = vim.api.nvim_get_current_line()
+	local text = vim.api.nvim_get_current_line()
 
-  local tag = findLastTag(text)
-  if tag then
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local tag = findLastTag(text)
+	if tag then
+		local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 
-    local insert_text = "</" .. tag .. ">"
-    vim.schedule(function()
-      vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { insert_text })
-      vim.api.nvim_win_set_cursor(0, { row, col })
-    end)
-    return ""
-  else
-    return ">"
-  end
+		local insert_text = "</" .. tag .. ">"
+		vim.schedule(function()
+			vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { insert_text })
+			vim.api.nvim_win_set_cursor(0, { row, col })
+		end)
+		return ""
+	else
+		return ">"
+	end
 end
 
-function M.setup()
-  vim.api.nvim_create_augroup("TagFileTypes", { clear = true })
+function M.setup(opts)
+	opts = opts or {}
+	local patterns = opts.patterns or { "*.tsx", "*.jsx", "*.html" }
 
-  vim.api.nvim_create_autocmd({ "TextChangedI", "InsertCharPre" }, {
-    group = "TagFileTypes",
-    pattern = "*",
-    callback = function()
-      local char = vim.v.char
-      if char == ">" then
-        vim.schedule(extract_last_html_tag)
-      end
-    end,
-  })
+	vim.api.nvim_create_augroup("TagFileTypes", { clear = true })
 
-  vim.api.nvim_create_user_command("ExtractLastHtmlTag", function()
-    extract_last_html_tag()
-  end, { nargs = 0 })
+	vim.api.nvim_create_autocmd({ "TextChangedI", "InsertCharPre" }, {
+		group = "TagFileTypes",
+		pattern = patterns,
+		callback = function()
+			local char = vim.v.char
+			if char == ">" then
+				vim.schedule(extract_last_html_tag)
+			end
+		end,
+	})
+
+	vim.api.nvim_create_user_command("ExtractLastHtmlTag", function()
+		extract_last_html_tag()
+	end, { nargs = 0 })
 end
 
 return M
