7c44e27604d5ecf44d517c9ebb61386a6f6496e6
Romulo Tone <romulo.tone@etraveligroup.com>
2024-07-01 09:30:11 +0200

    working but in all filetypes

--- lua/autotag/init.lua
+++ lua/autotag/init.lua
@@ -1,9 +1,5 @@
 local M = {}
 
-M.print_message = function()
-  print("Neoroma being founded")
-end
-
 local function findLastTag(text)
   local lastTag = nil
   for tag in text:gmatch("<(%w+([%.%w+]*))[^>]*") do
@@ -12,19 +8,17 @@ local function findLastTag(text)
   return lastTag
 end
 
--- Ensure this function is globally accessible
-function _G.extract_last_html_tag()
+local function extract_last_html_tag()
   local text = vim.api.nvim_get_current_line()
 
   local tag = findLastTag(text)
   if tag then
-    -- Get the current cursor position
     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 
-    local insert_text = "></" .. tag .. ">"
+    local insert_text = "</" .. tag .. ">"
     vim.schedule(function()
       vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { insert_text })
-      vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+      vim.api.nvim_win_set_cursor(0, { row, col })
     end)
     return ""
   else
@@ -32,20 +26,23 @@ function _G.extract_last_html_tag()
   end
 end
 
-M.setupAutoTag = function()
-  vim.api.nvim_buf_set_keymap(0, "i", ">", "v:lua.extract_last_html_tag()", { noremap = true, expr = true })
-end
-
 function M.setup()
-  vim.api.nvim_create_augroup("test", { clear = true })
+  vim.api.nvim_create_augroup("TagFileTypes", { clear = true })
 
-  vim.api.nvim_create_autocmd("FileType", {
-    group = "test",
-    pattern = "typescriptreact",
+  vim.api.nvim_create_autocmd({ "TextChangedI", "InsertCharPre" }, {
+    group = "TagFileTypes",
+    pattern = "*",
     callback = function()
-      M.setupAutoTag()
+      local char = vim.v.char
+      if char == ">" then
+        vim.schedule(extract_last_html_tag)
+      end
     end,
   })
+
+  vim.api.nvim_create_user_command("ExtractLastHtmlTag", function()
+    extract_last_html_tag()
+  end, { nargs = 0 })
 end
 
 return M
